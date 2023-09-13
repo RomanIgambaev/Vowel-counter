054517fe4f57672f546c0507b28f3ac7695e6da1
RomanIgambaev <roman.igambaev11@gmail.com>
2023-09-13 18:06:20 +0500

    vowel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    static int vowelCounter(String word) {
+        int vowelCount = 0;
+        String vowels = "aeiou";
+        for (char letter : word.toLowerCase().toCharArray()) {
+            if (vowels.contains(String.valueOf(letter))) {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слово:");
+        String userWord = scanner.nextLine();
+        System.out.println("Количество гласных букв: " + vowelCounter(userWord));
     }
 }
